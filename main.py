a2f248f1b27d08abf367e9aafa5ecd9aa6d3e00c
Eber L C Elias <71910158+ebertlc@users.noreply.github.com>
2023-07-11 22:31:49 -0300

    0.1.0

---
+++ main.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import pandas as pd
+
+print('....................................................................................')
+print('....................................................................................')
+print('............ B.E.L.A. - Bot de Exploração e Levantamento de Arquivos ...............')
+print('....................................................................................')
+print('....................................................................................')
+
+#....................................................................................
+#.............B.E.L.A. - Bot de Exploração e Levantamento de Arquivos................
+#....................................................................................
+
+# Configurar o driver do Firefox
+driver = webdriver.Firefox()
+#caminho_dataset = 'C:\\Users\\eber_\\Documents\\delveloper\\MIDR\\aquivos\\usuarios_desabilitar.xlsx'
+#usuarios = base_usuarios(caminho_dataset)
+
+# Acessar a página de pesquisa
+driver.get('https://homologacaos2id.mdr.gov.br/')
+
+botao_ok = driver.find_element(By.ID, "j_idt35")
+botao_ok.click()
+
+usuario_login = driver.find_element(By.ID, 'usuario')
+usuario_login.send_keys('email@dominio.br')
+
+senha_login = driver.find_element(By.ID, 'j_idt56')
+senha_login.send_keys('senha')
+
+botao_login = driver.find_element(By.ID, 'btnEnter')
+botao_login.click()
+
+time.sleep(5)
+
+sair = driver.find_element(By.ID, 'sair')
+sair.click()
