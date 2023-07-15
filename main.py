70f02698ac62522cf31f08c92f014f55c5343730
Eber L C Elias <71910158+ebertlc@users.noreply.github.com>
2023-07-15 15:56:54 -0300

    0.2.3

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+#....................................................................................
+#.............B.E.L.A. - Bot de Exploração e Levantamento de Arquivos................
+#....................................................................................
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
@@ -14,9 +18,6 @@ print('............ B.E.L.A. - Bot de Exploração e Levantamento de Arquivos ..
 print('....................................................................................')
 print('....................................................................................')
 
-#....................................................................................
-#.............B.E.L.A. - Bot de Exploração e Levantamento de Arquivos................
-#....................................................................................
 print('\n\n...................................Iniciando BELA...................................')
 # Configurar o driver do Firefox
 driver = webdriver.Firefox()
@@ -26,13 +27,15 @@ driver = webdriver.Firefox()
 # Acessar a página de pesquisa
 driver.get('https://s2id.mi.gov.br/paginas/index.xhtml')
 
+# Fecha a Caixa e Dialogo Inicial
 botao_ok = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "j_idt35")))
 botao_ok.click()
 
 print('\n\n.....................................S2iD Ativo.....................................')
 
+'
 
-
+# Fazer login
 usuario_login = driver.find_element(By.ID, 'usuario').send_keys(Login)
 senha_login = driver.find_element(By.ID, 'j_idt56').send_keys(Senha)
 time.sleep(1)
@@ -41,8 +44,7 @@ botao_login = driver.find_element(By.ID, 'btnEnter').click()
 
 print('\n...................................Usuario logado...................................')
 
-time.sleep(5)
-
+# Acessar o módulo "Reconhecimento Federal"
 rec_federal = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '[title="Reconhecimento Federal"]')))
 rec_federal.click()
 
@@ -51,8 +53,10 @@ print('\n..........................Entrando no modulo Reconhcimento.............
 # Iniciar o cronômetro
 start_time = time.time()
 
+# Definir o protocolo a ser pesquisado -  Por Enquanto
 protocolo = 'AL-F-2708501-13214-20230707'
 
+# Pesquisar o protocolo
 campo_pesquisa = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'accordion:form-reconhecimento:tbl-processos-reconhecimento:j_idt123:filter')))
 campo_pesquisa.clear()
 campo_pesquisa.send_keys(protocolo)
@@ -62,6 +66,7 @@ campo_pesquisa.send_keys(Keys.ENTER)
 print('\n.................................Pesquisa Concluida.................................')
 time.sleep(2)
 
+# Localizar a tabela de processos
 WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'accordion:form-reconhecimento:tbl-processos-reconhecimento_data')))
 
 tabela = driver.find_element(By.ID, 'accordion:form-reconhecimento:tbl-processos-reconhecimento_data')
@@ -70,10 +75,12 @@ time.sleep(1)
 
 print('\n................................Protocolo Localizado................................')
 
+# Acessar o detalhe do protocolo
 ActionChains(driver).double_click(tabela_linha).perform()
 
 print('\n................................Acessando  Protocolo................................')
 
+# Aguardar até o elemento onde os arquivos estão apareça
 detalhes = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'accordion:j_idt152')))
 
 #rolar até o fim da pagina
@@ -81,89 +88,112 @@ driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
 arquivos_disponiveis = ''
 
-
 print('\n.............................Procurando  RECONHECIMENTO.............................')
+# Iterar através de um range de 0 a 49 para percorrer as ids até Encontrar o pasta do Reconhecimento e expandi-la
 for pasta in range(0, 50):
     id = f"accordion:arquivos_disponiveis:{pasta}"
     print(f'......................................pasta  {pasta+1}......................................')
 
+    # Verifica se o id existe
     try:
         arquivo = detalhes.find_element(By.ID, id)
     except:
         continue
 
+    # Encontrar o elemento de tag que contém o texto do nó
     subarquivo = arquivo.find_element(By.TAG_NAME, 'span')
     tag = subarquivo.find_element(By.CLASS_NAME, 'ui-treenode-label')
+    # Verificar se o texto da tag é igual a 'RECONHECIMENTO'
     if tag.text == 'RECONHECIMENTO':
         print(f'\n........................RECONHECIMENTO Encontrado na Pasta {pasta + 1}........................')
         arquivos_disponiveis = id
+        # Encontrar o elemento de expansão para mostrar os subelementos
         reconhecimento = arquivo.find_element(By.TAG_NAME, 'span')
-        espandir = reconhecimento.find_element(By.CLASS_NAME, 'ui-tree-toggler')
-        espandir.click()
+        espandir = reconhecimento.find_element(By.CLASS_NAME, 'ui-tree-toggler').click()
+        # Sair do loop
         break
 time. sleep(2)
 
 print('\n.................................Procurando  DMATE..................................')
+# Iterar através de um range de 0 a 49 para percorrer as ids até Encontrar o pasta do DMATE e marca-la
 for pasta in range(0, 50):
     id = f"{arquivos_disponiveis}_{pasta}"
     print(f'......................................pasta  {pasta+1}......................................')
 
+    # Verifica se o id existe
     try:
         arquivo = detalhes.find_element(By.ID, id)
     except:
         continue
 
+    # Encontrar o elemento de tag que contém o texto do nó
     subarquivo = arquivo.find_element(By.TAG_NAME, 'span')
     tag = subarquivo.find_element(By.CLASS_NAME, 'ui-treenode-label')
+    # Verificar se o texto da tag é igual a 'DMATE'
     if tag.text == 'DMATE':
         print(f'............................DMATE Encontrado na Pasta {pasta+1}.............................')
+        # Encontrar o elemento de checkbox para marcar os subelementos
         checkbox = arquivo.find_element(By.TAG_NAME, 'span')
         checkbox_cls = checkbox.find_element(By.CLASS_NAME, 'ui-chkbox-icon')
         checkbox_cls.click()
+        # Sair do loop
         break
 
 print('\n..................................Procurando FIDE...................................')
+# Iterar através de um range de 0 a 49 para percorrer as ids até Encontrar o pasta do FIDE e marca-la
 for pasta in range(0, 50):
     id = f"{arquivos_disponiveis}_{pasta}"
     print(f'......................................pasta  {pasta+1}......................................')
 
+    # Verifica se o id existe
     try:
         arquivo = driver.find_element(By.ID, id)
     except:
         continue
 
+    # Encontrar o elemento de tag que contém o texto do nó
     subarquivo = arquivo.find_element(By.TAG_NAME, 'span')
     tag = subarquivo.find_element(By.CLASS_NAME, 'ui-treenode-label')
+    # Verificar se o texto da tag é igual a 'FIDE'
     if tag.text == 'FIDE':
         print(f'............................FIDE Encontrado na Pasta {pasta+1}..............................')
+        # Encontrar o elemento de checkbox para marcar os subelementos
         checkbox = arquivo.find_element(By.TAG_NAME, 'span')
         checkbox_cls = checkbox.find_element(By.CLASS_NAME, 'ui-chkbox-icon')
         checkbox_cls.click()
+        # Sair do loop
         break
 
 time.sleep(1)
 
+# Gerar o PDF
 driver.find_element(By.ID, 'accordion:j_idt161').click()
 time.sleep(1)
 
+# Localizar o elemento "object" e mudar para o frame
 canva = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'object')))
 
 print('\n....................................PDF  GERADO.....................................')
 
 time.sleep(1)
 
+# Mudar para o frame do objeto "object"
 driver.switch_to.frame(canva)
+# Isso permite interagir com elementos dentro do frame
 
+# Localizar o elemento de download dentro da parte direita do visualizador de ferramentas e clicar nele
 container = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'mainContainer')))
 toolbar = container.find_element(By.ID, 'toolbarContainer')
 toolbar_viewer = toolbar.find_element(By.ID, 'toolbarViewer')
 toolbar_viewer_direita = toolbar_viewer.find_element(By.ID, 'toolbarViewerRight')
 download = toolbar_viewer_direita.find_element(By.ID, 'download').click()
 
+# Mudar para o conteúdo padrão da página
 driver.switch_to.default_content()
+# Isso é necessário para interagir com elementos fora do frame
 
 time.sleep(3)
-
+#definir o caminho onde será salvo e o nome do aqruivo
 caminho = f'C:\\Users\\eber_\\Documents\\S2iD\\teste\\FIDE-DEMATE_municipio_{protocolo}_status3.pdf'
 
 app = Application(backend="win32").connect(title='Salvar como')
@@ -177,6 +207,7 @@ filename_input.type_keys('{ENTER}')  # Digita a tecla Enter para ativar o botão
 
 print('\n...................................Arquivo Salvo....................................')
 
+# Voltar para a página de pesquisa
 botao_voltar = driver.find_element(By.ID, 'j_idt26').click()
 
 print('\n............................Voltar a Pagina de Pesquisa.............................')
@@ -187,6 +218,7 @@ execution_time = end_time - start_time
 # Imprimir o tempo de execução
 print(f"Tempo de execução: {execution_time} segundos")
 
+# Deslogar do sistema
 time.sleep(10)
 sair = driver.find_element(By.ID, 'sair').click()
 
